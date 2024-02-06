72fafb5d5a2304b6d80b637619be8d37005a8f4f
htkwon <a3001sd@gmail.com>
2024-02-06 11:00:51 +0900

    API 추가
    - (title, content, username 필드에서) parameter 단어 검색
    - (title, content, username 필드에서)
    parameter 단어 포함 검색

--- src/main/java/com/example/project/controller/ArticleController.java
+++ src/main/java/com/example/project/controller/ArticleController.java
@@ -1,6 +1,5 @@
 package com.example.project.controller;
 
-import com.example.project.dto.ArticleAllSaveDto;
 import com.example.project.dto.ArticleResponseDto;
 import com.example.project.dto.SearchDto;
 import com.example.project.service.ArticleService;
@@ -18,42 +17,66 @@ public class ArticleController {
 
     private final ArticleService articleService;
 
-
-    @PostMapping("/articles")
-    public ResponseEntity<Void> saveAll(@RequestBody List<ArticleAllSaveDto> dto){
-        articleService.saveAllArticle(dto);
-        return ResponseEntity.ok().build();
-    }
-
+    /**
+     * JPA Data -> Elasticsearch SAVE
+     */
     @PostMapping("/articleDocuments")
     public ResponseEntity<Void> saveArticleDocuments(){
         articleService.saveAllArticleDocument();
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * Elasticsearch title 검색
+     */
     @GetMapping("/article/title")
     public ResponseEntity<List<ArticleResponseDto>> searchByTitle(@RequestParam String title){
         List<ArticleResponseDto> res = articleService.findByTitle(title);
         return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 
+    /**
+     * Elasticsearch content 검색
+     */
     @GetMapping("/article/content")
     public ResponseEntity<List<ArticleResponseDto>> searchByContent(@RequestParam String content){
         List<ArticleResponseDto> res = articleService.findByContent(content);
         return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 
+    /**
+     * Elasticsearch 전체 Document 삭제
+     */
     @DeleteMapping("/articleDocuments/delete")
     public ResponseEntity<Void> deleteArticleDocument(){
         articleService.deleteArticleDocument();
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * 조건 검색
+     */
     @GetMapping("/article/search")
     public ResponseEntity<List<ArticleResponseDto>> searchBySearchDto(@RequestBody SearchDto searchDto){
         return ResponseEntity.ok(articleService.findBySearchDto(searchDto));
     }
 
+    /**
+     * (title,content,username FIELD 에서) word 단어 검색
+     */
+    @GetMapping("/article/search/all")
+    public ResponseEntity<List<ArticleResponseDto>> searchByWord(@RequestParam String word){
+        List<ArticleResponseDto> res = articleService.findByWord(word);
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
 
+    /**
+     * (title,content,username FIELD 에서) word 단어 포함 검색
+     */
+    @GetMapping("/article/search/contain")
+    public ResponseEntity<List<ArticleResponseDto>> searchContainWord(@RequestParam String word){
+        List<ArticleResponseDto> res = articleService.findByContainWord(word);
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
 
 }
